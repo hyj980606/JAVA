2d0421a999a59a1a0ca88280116162208388accd
hyj980606 <1841406870@qq.com>
2022-07-17 19:56:42 +0800

    数组

--- src/com/myArray/ArrayTest02.java
+++ src/com/myArray/ArrayTest02.java
@@ -11,38 +11,39 @@ package com.myArray;
 
 public class ArrayTest02 {
     public static void main(String[] args) {
-        //定义两个数组
-        int[] arr1 = new int[2];
-        int[] arr2 = new int[3];
+        //定义一个数组
+        int[] arr = new int[3];
 
-        //分别输出数组名及元素
-        System.out.println(arr1);
-        System.out.println(arr1[0]);
-        System.out.println(arr1[1]);
+        //赋值
+        arr[0] = 100;
+        arr[1] = 200;
+        arr[2] = 300;
+
+        //输出
+        System.out.println(arr);
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println("---------------------");
+
+        //然后定义第二个数组的时候把第一个数组的地址赋值给第二个数组，
+        int[] arr2 = arr;
+
+        //给第二个数组赋值
+        arr2[0] = 111;
+        arr2[1] = 222;
+        arr2[2] = 333;
+
+        //再次输出两个数组的名级及元素
+        System.out.println(arr);
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println("---------------------");
 
         System.out.println(arr2);
         System.out.println(arr2[0]);
         System.out.println(arr2[1]);
         System.out.println(arr2[2]);
-
-        //然后分别给数组中的元素赋值
-        arr1[1] = 100;
-
-        arr2[0] = 200;
-        arr2[2] = 300;
-
-        //分别再次输出数组名及元素
-        System.out.println(arr1);
-        System.out.println(arr1[0]);
-        System.out.println(arr1[1]);
-
-        System.out.println(arr2);
-        System.out.println(arr2[0]);
-        System.out.println(arr2[1]);
-        System.out.println(arr2[2]);
-        System.out.println("-------------------");
-
-
-
     }
 }
