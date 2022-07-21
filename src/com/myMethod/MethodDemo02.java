8d6e80a49eb8ebbe47ad261d8877f293b02c2c4d
hyj980606 <1841406870@qq.com>
2022-07-21 10:30:01 +0800

    方法调用

---
+++ src/com/myMethod/MethodDemo02.java
@@ -0,0 +1,18 @@
+package com.myMethod;
+
+/**
+ * @ClassName MethodDemo02
+ * @Description 方法的调用：（有明确返回值的方法的调用）
+ *                  A：单独调用，没有意义
+ *                  B：输出调用，有意义，但是不够好，因为我可能需要拿出结果进一步的操作
+ *                  C：赋值调用，推荐方式
+ * @Author hyj
+ * @Date 2022-07-20 20:57
+ * @Version 1.0
+ */
+
+public class MethodDemo02 {
+    public static void main(String[] args) {
+
+    }
+}
